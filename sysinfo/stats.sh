ca2a34abd36d145ea6fcd30ec9c8d01ada996f5b
allezwiggo <manas4apps@gmail.com>
2021-04-23 12:56:39 +0800

    Path var changes.

--- sysinfo/stats.sh
+++ sysinfo/stats.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-TEST_NAME=$(cat test.cfg | grep TEST_NAME | cut -d'=' -f2)
+####### CHANGE THIS ########
+HOME_DIR=/home/centos/sysinfo
+############################
+TEST_NAME=$(cat $HOME_DIR/test.cfg | grep TEST_NAME | cut -d'=' -f2)
 TS="$(date +'%Y-%m-%d %H:%M:%S')"
-HOME_DIR=$(cat test.cfg | grep HOME_DIR | cut -d'=' -f2)
-DATA_DIR=$(cat test.cfg | grep DATA_DIR | cut -d'=' -f2)
+DATA_DIR=$(cat $HOME_DIR/test.cfg | grep DATA_DIR | cut -d'=' -f2)
 
 if [ ! -f "$DATA_DIR/$TEST_NAME" ]; then
   mkdir -p "$DATA_DIR/$TEST_NAME"
 fi
 
-./machine.sh "$DATA_DIR/$TEST_NAME"
-./fs.sh "$TS" "$DATA_DIR/$TEST_NAME"
-./top.sh "$TS" "$DATA_DIR/$TEST_NAME"
-./sys_util.sh "$TS" "$DATA_DIR/$TEST_NAME"
-./disk.sh "$TS" "$DATA_DIR/$TEST_NAME"
+$HOME_DIR/machine.sh "$DATA_DIR/$TEST_NAME"
+$HOME_DIR/fs.sh "$TS" "$DATA_DIR/$TEST_NAME"
+$HOME_DIR/top.sh "$TS" "$DATA_DIR/$TEST_NAME"
+$HOME_DIR/sys_util.sh "$TS" "$DATA_DIR/$TEST_NAME"
+$HOME_DIR/disk.sh "$TS" "$DATA_DIR/$TEST_NAME"
 
 for fl in addons/*.sh
 do
-  ./$fl "$TS" "$HOME_DIR" "$DATA_DIR/$TEST_NAME"
+  $HOME_DIR/$fl "$TS" "$HOME_DIR" "$DATA_DIR/$TEST_NAME"
 done
