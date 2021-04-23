9bdd9bfbd7a89302010a4c571b2a41112f92939a
allezwiggo <manas4apps@gmail.com>
2021-04-23 10:50:37 +0800

    Fixed quotes for ts.

--- sysinfo/stats.sh
+++ sysinfo/stats.sh
@@ -2,14 +2,15 @@
 TEST_NAME=$(cat test.cfg | grep TEST_NAME | cut -d'=' -f2)
 TS="$(date +'%Y-%m-%d %H:%M:%S')"
 DATA_BASE=$(cat test.cfg | grep DATA_DIR | cut -d'=' -f2)
-DATA_DIR="$DATA_BASE/$TEST_NAME"
 
+echo $DATA_DIR
 if [ ! -f $DATA_DIR ]; then
   mkdir -p $DATA_DIR
 fi
+
 ./machine.sh "$DATA_DIR"
-./fs.sh $TS "$DATA_DIR"
-./top.sh $TS "$DATA_DIR"
+./fs.sh "$TS" "$DATA_DIR"
+./top.sh "$TS" "$DATA_DIR"
 ./sys_util.sh "$TS" "$DATA_DIR"
 ./disk.sh "$TS" "$DATA_DIR"
 
